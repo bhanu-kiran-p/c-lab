161aac0477d5338d1078398c45444eacb99f6a25
Bhanu Kiran <bhanukiranpenubothus@gmail.com>
2026-08-27 16:17:00 +0530

    added new files

---
+++ worksheet-4/p14a_patterns.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int n, i, j;
+    n = 5;
+    for(i=1; i<=n; i++){
+        for(j=1; j<i; j++){
+            printf(" ");
+        }
+        for(j=1; j<=2*(n-i)+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=1; i<=n-1; i++){
+        for(j=1;j<=n-1-i; j++){
+            printf(" ");
+        }
+        for(j=1; j<=2*i+1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ worksheet-4/p15_tables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i, j, n;
+    n = 10;
+    printf("MULTIPLICATION TABLE\n");
+    printf("--------------------------------------------------------------------------------------------------\n");
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n; j++){
+            printf("%d\t", i*j);
+        }
+        printf("\n");
+    }
+}
