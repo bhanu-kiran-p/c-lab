0b98e00f09b41d0196b60c06f1fa7c41648e242a
Bhanu Kiran Penubothu <bhanukiranpenubothus@gmail.com>
2026-09-08 22:20:11 +0530

    add files

---
+++ week-mic_practice/biggest_among_5_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c,d,e;
+    printf("Enter five numbers: ");
+    scanf("%d%d%d%d%d", &a,&b,&c,&d,&e);
+    if(a>b && a>c && a>d && c &&a>d &&a>e)
+        printf("%d is the largest number", a);
+    return 0;
+}
