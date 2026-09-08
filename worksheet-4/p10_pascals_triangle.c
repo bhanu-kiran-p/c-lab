33822eb3798ff03054605b038b76428b774bef67
Bhanu Kiran Penubothu <bhanukiranpenubothus@gmail.com>
2026-09-09 00:20:01 +0530

    add files

--- worksheet-4/p10_pascals_triangle.c
+++ worksheet-4/p10_pascals_triangle.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
-int main(){
-    int n = 5, i, j;
-    for(i)
+
+int main() {
+    int n, i, j;
+    int num;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        num = 1;
+
+        // Print spaces for formatting
+        for (j = 0; j < n - i - 1; j++) {
+            printf(" ");
+        }
+
+        // Print Pascal Triangle values
+        for (j = 0; j <= i; j++) {
+            printf("%d ", num);
+            num = num * (i - j) / (j + 1);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
 }
