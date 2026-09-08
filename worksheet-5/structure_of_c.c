3a8c127613602b9e3102f2658fd1b35fa51ac6de
Bhanu Kiran Penubothu <bhanukiranpenubothus@gmail.com>
2026-09-08 22:07:26 +0530

    add new files

---
+++ worksheet-5/structure_of_c.c
@@ -0,0 +1,28 @@
+/*
+this is the documentation section
+name : bhanu kiran
+date : --/--/----
+purpuse : to find the structure of c language
+*/
+
+// linkage section
+#include <stdio.h>
+
+// definintion section
+# define A 10
+
+// global declaration
+int b;
+
+// function prototype
+int sum(int);
+
+// main section
+int main(){
+
+}
+
+// sub - routine section
+sum(int b){
+    printf("%d", A+b);
+}
